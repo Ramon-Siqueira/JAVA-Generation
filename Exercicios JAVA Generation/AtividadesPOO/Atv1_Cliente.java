cf2b627e65a7a52873f6a11db13fe930ef35865f
unknown <ramonsiqueira80@gmail.com>
2023-04-25 14:08:10 -0300

    versão 9

---
+++ Exercicios JAVA Generation/AtividadesPOO/Atv1_Cliente.java
@@ -0,0 +1,80 @@
+package AtividadesPOO;
+
+public class Atv1_Cliente {
+
+	private String nomeCliente;
+	private String Email;
+	private String datanasc;
+	private int CPF;
+	private int celular;
+	
+	
+	public Atv1_Cliente(String nomeCliente, String email, String datanasc, int cPF, int celular) {
+		super();
+		this.nomeCliente = nomeCliente;
+		Email = email;
+		this.datanasc = datanasc;
+		CPF = cPF;
+		this.celular = celular;
+	}
+
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+
+	public String getEmail() {
+		return Email;
+	}
+
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
+
+	public String getDatanasc() {
+		return datanasc;
+	}
+
+
+	public void setDatanasc(String datanasc) {
+		this.datanasc = datanasc;
+	}
+
+
+	public int getCPF() {
+		return CPF;
+	}
+
+
+	public void setCPF(int cPF) {
+		CPF = cPF;
+	}
+
+
+	public int getCelular() {
+		return celular;
+	}
+
+
+	public void setCelular(int celular) {
+		this.celular = celular;
+	}
+
+
+	public void visualizar() {
+		System.out.println("O cliente "+nomeCliente+" possui o E-mail "+Email+" Seu CPF é "
+	+CPF+", Sua Data de Nascimento é "+datanasc+" e por fim seu número de telefone "+celular);
+		
+		System.out.println("\nSegue como solicitado nome do cliente e seu telefone: "+nomeCliente+", "+celular);
+	}
+	
+}
+	
---
+++ Exercicios JAVA Generation/AtividadesPOO/testaCliente.java
@@ -0,0 +1,14 @@
+package AtividadesPOO;
+
+public class testaCliente {
+
+	public static void main(String[] args) {
+		
+		Atv1_Cliente Cli = new Atv1_Cliente (" Ramon ","ramon123@gmail.com, ","14/03/1995" , 1234567890 , 1191234567 );
+		Cli.getNomeCliente();
+		Cli.getCelular();
+		Cli.visualizar();
+	}
+	
+
+}
