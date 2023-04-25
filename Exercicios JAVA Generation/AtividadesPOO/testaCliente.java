0772917fed1ae30a43c249bc524ba00fda6b56c7
Ramon-Siqueira <103185015+Ramon-Siqueira@users.noreply.github.com>
2023-04-25 14:33:23 -0300

    Update testaCliente.java

--- Exercicios JAVA Generation/AtividadesPOO/testaCliente.java
+++ Exercicios JAVA Generation/AtividadesPOO/testaCliente.java
@@ -5,8 +5,8 @@ public class testaCliente {
 	public static void main(String[] args) {
 		
 		Atv1_Cliente Cli = new Atv1_Cliente (" Ramon ","ramon123@gmail.com, ","14/03/1995" , 1234567890 , 1191234567 );
-		Cli.getNomeCliente();
-		Cli.getCelular();
+		Atv1_Cliente Cli2 = new Atv1_Cliente (" Siqueira ","siqueira123@gmail.com, ","03/03/2002" , 1234567890 , 1191234567 );
+		Cli.visualizar();
 		Cli.visualizar();
 	}
 	
